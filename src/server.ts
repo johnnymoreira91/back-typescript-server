0d7c03645c5f935836dbd91bbc9298068cdb3bb3
Johnny Moreira <jmoreira@engebras.com.br>
2022-01-16 21:08:05 -0300

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'hello' })
+app.get('/', (req, res) => res.json({ message: 'hello' }));
+
+app.get('/teste', (req, res) => {
+  res.status(200).json({ message: 'teste' });
 });
 
 app.listen(3000, () => {
-  console.log('running')
+  console.log('running');
 });
